5a2617ee18f33ee457a355821e4901a716d0c21b
Sidney Cadot <sidney@jigsaw.nl>
2019-09-23 19:41:40 +0200

    Improved dump_tables.

--- dump_tables.py
+++ dump_tables.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 
-"""This script dumps the F1 2019 telemetry tables in RST markup format."""
+"""This script dumps the F1 2019 telemetry tables in RST or MarkDown format."""
 
-from f1_2019_telemetry_packets import PacketID, TeamIDs, DriverIDs, TrackIDs, NationalityIDs, SurfaceTypes, ButtonFlag, EventStringCode
+import argparse
 
-def dump_table(items, labels, num_rows=None, num_cols=None):
+from f1_2019_telemetry import PacketID, TeamIDs, DriverIDs, TrackIDs, NationalityIDs, SurfaceTypes, ButtonFlag, EventStringCode
+
+def dump_table_rst(items, labels, num_rows=None, num_cols=None):
     """Dump a number of items as an RST markup table.
 
     Args:
@@ -63,8 +65,61 @@ def dump_table(items, labels, num_rows=None, num_cols=None):
         print(separator_line_1)
 
 
-def main():
-    """Dump F1 2019 tables in RST format."""
+def dump_table_markdown(items, labels, num_rows=None, num_cols=None):
+    """Dump a number of items as a MarkDown table.
+
+    Args:
+        items: a sequence of n-element items, to ne included in the table body.
+        labels: an optional sequence of n strings to be used as headers.
+        num_rows: number of rows to be used. Defaults to the length of the 'items' sequence.
+        num_cols: number of 'major' columns (each consisting of n 'minor' columns).
+    """
+
+    if num_rows is None:
+        num_rows = len(items)
+
+    if num_cols is None:
+        num_cols = (len(items) + num_rows - 1) // num_rows
+
+    labels = [str(label) for label in labels]
+    items  = [tuple(str(x) for x in item) for item in items]
+
+    # Determine column widths
+    column_widths = []
+    for col in range(num_cols):
+        widths = (len(x) for x in labels)
+        for row in range(num_rows):
+            idx = num_rows * col + row
+            if idx < len(items):
+                widths = tuple(max(w, len(x)) for (w, x) in zip(widths, items[idx]))
+        column_widths.append(widths)
+
+    # Ready to print
+    separator_line_1 = "|" + "|".join("|".join(" " + w * "-" + " " for w in widths) for widths in column_widths) + "|"
+
+    if labels is not None:
+        print("|", end='')
+        for (col, widths) in enumerate(column_widths):
+            for (label, width) in zip(labels, widths):
+                print(" " + label + (width - len(label) + 1) * " ", end='')
+                print("|", end='')
+        print()
+
+        print(separator_line_1)
+
+    for row in range(num_rows):
+
+        print("|", end='')
+        for (col, widths) in enumerate(column_widths):
+            idx = num_rows * col + row
+            values = items[idx] if idx < len(items) else ("", "")
+            for (value, width) in zip(values, widths):
+                print(" " + value + (width - len(value) + 1) * " ", end='')
+                print("|", end='')
+        print()
+
+
+def dump_tables(dump_table_func):
 
     # This table is in the spec as given on the CodeMaster forum, but not in the module.
     # Note that we added the 'uint32' type which is missing from the spec.
@@ -78,36 +133,54 @@ def main():
         ( "uint64" , "Unsigned 64-bit integer" )
     ]
 
-    dump_table(TypesAndDescriptionsTable, ["Type", "Description"])
+    dump_table_func(TypesAndDescriptionsTable, ["Type", "Description"])
     print()
 
     PacketIDTable = [(PacketID.short_description[p], p.value, PacketID.long_description[p]) for p in PacketID]
-    dump_table(PacketIDTable, ["Packet Name", "Value", "Description"])
+    dump_table_func(PacketIDTable, ["Packet Name", "Value", "Description"])
     print()
 
     EventStringCodeTable = [(EventStringCode.short_description[e], e.value.decode(), EventStringCode.long_description[e]) for e in EventStringCode]
-    dump_table(EventStringCodeTable, ["Event", "Code", "Description"])
+    dump_table_func(EventStringCodeTable, ["Event", "Code", "Description"])
     print()
 
-    dump_table(sorted(TeamIDs.items()), ["ID", "Team"], 20)
+    dump_table_func(sorted(TeamIDs.items()), ["ID", "Team"], 20)
     print()
 
-    dump_table(sorted(DriverIDs.items()), ["ID", "Driver"], 30)
+    dump_table_func(sorted(DriverIDs.items()), ["ID", "Driver"], 30)
     print()
 
-    dump_table(sorted(TrackIDs.items()), ["ID", "Track"])
+    dump_table_func(sorted(TrackIDs.items()), ["ID", "Track"])
     print()
 
-    dump_table(sorted(NationalityIDs.items()), ["ID", "Nationality"], 30)
+    dump_table_func(sorted(NationalityIDs.items()), ["ID", "Nationality"], 30)
     print()
 
-    dump_table(sorted(SurfaceTypes.items()), ["ID", "Surface"])
+    dump_table_func(sorted(SurfaceTypes.items()), ["ID", "Surface"])
     print()
 
     ButtonFlagTable = [("0x{:04x}".format(k), v) for (k, v) in sorted((bf.value, ButtonFlag.description[bf]) for bf in ButtonFlag)]
-    dump_table(ButtonFlagTable, ["Bit flags", "Button"])
+    dump_table_func(ButtonFlagTable, ["Bit flags", "Button"])
     print()
 
+def main():
+
+    """Dump F1 2019 tables in RST or MarkDown format."""
+
+    argparser = argparse.ArgumentParser
+
+    parser = argparse.ArgumentParser(description='Dump F1 2019 tables in format suitable for processing.')
+    parser.add_argument('-f', '--format', default='rst', choices=['rst', 'markdown'],
+                        help='Format of tables to be dumped (default: rst).')
+
+    args = parser.parse_args()
+
+    if args.format == 'rst':
+        dump_table_func = dump_table_rst
+    else:
+        dump_table_func = dump_table_markdown
+
+    dump_tables(dump_table_func)
 
 if __name__ == "__main__":
     main()
